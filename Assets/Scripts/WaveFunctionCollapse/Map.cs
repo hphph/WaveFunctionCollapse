c64bde162d90838b7e2692f5460b3fcff5104eca
hphph <hphph2@gmail.com>
2024-07-07 16:06:19 +0200

    Map.cs change and project update

--- Assets/Scripts/WaveFunctionCollapse/Map.cs
+++ Assets/Scripts/WaveFunctionCollapse/Map.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class Map
 {
-    Slot[] slots;
-    Vector2Int mapSize;
+	Slot[] slots;
+	Vector2Int mapSize;
 
-    public Vector2Int MapSize => mapSize;
+	public Vector2Int MapSize => mapSize;
 
-    int GetMapIndex(Vector2Int position) => position.y * mapSize.x + position.x;
-	Vector2Int GetPositionFromIndex(int index) => new Vector2Int(index%mapSize.x, index/mapSize.x);
+	int GetMapIndex(Vector2Int position) => position.y * mapSize.x + position.x;
+	Vector2Int GetPositionFromIndex(int index) => new Vector2Int(index % mapSize.x, index / mapSize.x);
 
 
-    public Map(Vector2Int mapSize, Module[] modules)
-    {
-        this.mapSize = mapSize;
-        slots = new Slot[mapSize.x * mapSize.y];
-		for(int i = 0; i < slots.Length; i++)
+	public Map(Vector2Int mapSize, Module[] modules)
+	{
+		this.mapSize = mapSize;
+		slots = new Slot[mapSize.x * mapSize.y];
+		for (int i = 0; i < slots.Length; i++)
 		{
 			slots[i] = new Slot(modules);
 		}
-    }
+	}
 
 	public Slot GetSlot(Vector2Int position)
 	{
 		int moduleIndex = GetMapIndex(position);
-		if(moduleIndex >= 0 && moduleIndex < slots.Length)
+		if (moduleIndex >= 0 && moduleIndex < slots.Length)
 		{
 			return slots[moduleIndex];
 		}
@@ -36,7 +36,7 @@ public class Map
 	public Vector2Int GetPosition(Slot slot)
 	{
 		int index = Array.FindIndex<Slot>(slots, s => s == slot);
-		if(index == -1) Debug.LogError("Slot not in map jurisdiction");
+		if (index == -1) Debug.LogError("Slot not in map jurisdiction");
 		return GetPositionFromIndex(index);
 	}
 }
